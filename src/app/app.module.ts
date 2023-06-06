1bb960c1bbc1a0f67d2989962c35687510a11f4f
Lii02 <lukeinlow@gmail.com>
2023-06-06 13:05:49 -0400

    Base component structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TitlebarComponent } from './titlebar/titlebar.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlebarComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

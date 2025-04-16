c83ed3b4fffffce862cdffabd726aa9d20137f80
Luke Inlow <lukeinlow@gmail.com>
2025-04-15 22:22:24 -0400

    removed project from current

--- src/routes/current.tsx
+++ src/routes/current.tsx
@@ -4,12 +4,6 @@ export default function Current() {
 	return (
 		<section className='Projects'>
 			<Project
-				name='MyMinis'
-				description='A full stack app, for maintaining and keeping track of miniatures. Written in TypeScript on the frontend and backend.'
-				year={2025}
-				link='https://myminis.vercel.app/'
-			/>
-			<Project
 				name='liTech'
 				description='An OpenGL game engine written in C++.'
 				year={2025}
